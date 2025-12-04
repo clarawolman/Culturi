63e1f2624592dca1f844b6f7d7a8fcb474f51f07
CLARA WOLMAN <49359853@ort>
2025-12-04 10:08:12 -0300

    rami commit 2

--- obj/Debug/net8.0/PrimerProyecto.AssemblyInfo.cs
+++ obj/Debug/net8.0/PrimerProyecto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimerProyecto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ef234222723ac66909f590ad74180189688dda5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eff8fadc2bbe9d24ddc6be64cae2387f5f41a6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimerProyecto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimerProyecto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
